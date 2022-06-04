dc8e09fb3b7cfbbf6e1f49eb7c74e6bf463f88ad
younglei <youngleish@gmail.com>
2022-06-04 23:56:07 +0800

    feat(v0.1.0): support vite serve

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import {
   normalizePath,
 } from 'vite'
 import { cleanUrl } from 'vite-plugin-utils'
+import template from 'lodash.template'
 
 export interface Options {
   /** Value of script src */
-  entry?: string
+  inject?: string
   /**
    * Path of [name].html
    * 
@@ -16,32 +17,59 @@ export interface Options {
    * - { 'index.html': 'public/index.ejs' }
    */
   template?: string | { [entryAlias: string]: string }
+  data?: Record<string, any>
+
+  /** Finally value of `req.url` */
+  _url?: string
 }
 
 export default function viteHtml(options: Options | Options[] = {}): Plugin[] {
-  const opts = Array.isArray(options) ? options : [options]
+  const opts = mappingTemplate(Array.isArray(options) ? options : [options])
   let config: ResolvedConfig
+
+  const plugin: Plugin = {
+    name: 'vite-html',
+    configResolved(_config) {
+      config = _config
+    },
+    transformIndexHtml(html, ctx) {
+      const opt = opts.find(opt => opt._url === ctx.path) || {}
+
+      // Inject js
+      if (opt.inject) {
+        // TODO: inject js to anywhere
+        html = html.replace(
+          '</body>',
+          `  <script src="${opt.inject}" type="module"></script>\n  </body>`,
+        )
+      }
+
+      // It's ejs template
+      if (html.includes('<%')) {
+        html = template(html)(opt.data)
+      }
+
+      return html
+    },
+  }
 
   return [
     {
-      name: 'vite-html',
+      ...plugin,
       apply: 'serve',
-      configResolved(_config) {
-        config = _config
-      },
       configureServer(server) {
         server.middlewares.use((req, res, next) => {
           let url = req.url ? cleanUrl(req.url) : ''
           if (url === '/') {
             url = '/index.html'
           }
-          const opt = opts.find(
-            opt => opt.template && mappingTemplate(opt.template)[url.slice(1)],
-          )
+          const opt = opts.find(opt => opt.template && opt.template[url.slice(1)])
           if (opt) {
             const [, template] = Object.entries(opt.template)[0]
             // `template.replace()` for support absolute path
             req.url = normalizePath('/' + template.replace(config.root, ''))
+            // Useful in `transformIndexHtml` hook
+            opt._url = req.url
           }
 
           next()
@@ -49,19 +77,22 @@ export default function viteHtml(options: Options | Options[] = {}): Plugin[] {
       },
     },
     {
-      name: 'vite-html',
+      ...plugin,
       apply: 'build',
       // TODO: vite build
     }
   ]
 }
 
-// Mapping 'public/[name].html' to { '[name].html': 'public/[name].html' }
-function mappingTemplate(template: Options['template']): Record<string, string> {
-  if (typeof template !== 'string') {
-    return template
-  }
-  const lastIndex = template.lastIndexOf('/')
-  const name = lastIndex > -1 ? template.slice(lastIndex + 1) : template
-  return { [name]: template }
+function mappingTemplate(options: Options[]): Options[] {
+  return options.map(opts => {
+    const { template } = opts
+    if (typeof template === 'string') {
+      const lastIndex = template.lastIndexOf('/')
+      const name = lastIndex > -1 ? template.slice(lastIndex + 1) : template
+      // Mapping 'public/[name].html' to { '[name].html': 'public/[name].html' }
+      opts.template = { [name]: template }
+    }
+    return opts
+  })
 }
